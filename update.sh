66c6e880fe326d08ba989d94aaf04b1d67c96ac3
Anatoly Sukhanov <anatoly.sukhanov@gmail.com>
2018-12-10 13:03:44 +0000

    make changes to diff update script

--- update.sh
+++ update.sh
@@ -1,11 +1,13 @@
-WORKOSM_DIR=/home/osm
+WORKOSM_DIR=/home/data
 PGHOST=localhost
 #PGUSER=postgres
 #PGPASSWORD=postgres
-#for f in $WORKOSM_DIR/*; do
-    #if [ -d ${f} ]; then
-        #echo "$(basename "$f")"
-    #fi	
-#done
-osmosis --read-replication-interval workingDirectory="${WORKOSM_DIR}" --simplify-change --write-xml-change
-osm2pgsql --cache 300 -s -a -l -d osm -U postgres -H $PGHOST change.osc
+
+for f in $WORKOSM_DIR/*; do
+    if [ -d ${f} ]; then
+	country=$(basename "$f")
+	cd $WORKOSM_DIR/$country
+        osmosis --read-replication-interval workingDirectory="." --simplify-change --write-xml-change
+        osm2pgsql --cache 300 -s -a -l -d osm -U postgres -H $PGHOST change.osc
+    fi
+done
